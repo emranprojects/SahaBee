068041500a7f98af8cedae359b403420c9bddf38
emran <emran.bm@gmail.com>
2020-04-11 21:48:52 +0430

    Add production host to allowed hosts
    + sum refactorings

--- sahabee/settings-production.py
+++ sahabee/settings-production.py
@@ -1,6 +1,9 @@
 from sahabee.settings import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-SECRET_KEY = os.environ.get('SAHABEE_SECRET_KEY', 'dummy')
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    os.environ.get('PRODUCTION_IP')]
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dummy')
 DATABASES['default']['NAME'] = '/database/db.sqlite3'
