3dd2cc7ba81163a8a46f5164eff87f942f7f1d1c
emran <emran.bm@gmail.com>
2020-04-12 22:54:42 +0430

    Disable CORS

--- sahabee/settings-production.py
+++ sahabee/settings-production.py
@@ -1,9 +1,6 @@
 from sahabee.settings import *
 
 DEBUG = False
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    os.environ.get('PRODUCTION_IP')]
+ALLOWED_HOSTS = ['*']
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dummy')
 DATABASES['default']['NAME'] = '/database/db.sqlite3'
