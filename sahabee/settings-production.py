b55b34c6898f892d3bd7eb2db63d41c47f93b4dc
emran <emran.bm@gmail.com>
2020-04-12 23:11:23 +0430

    Enable debugging on production

--- sahabee/settings-production.py
+++ sahabee/settings-production.py
@@ -1,6 +1,6 @@
 from sahabee.settings import *
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dummy')
 DATABASES['default']['NAME'] = '/database/db.sqlite3'
