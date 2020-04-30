27d013493eabf0f9a982939974ea47390d949d5f
emran <emran.bm@gmail.com>
2020-04-30 17:37:29 +0430

    Feature: Add UserDetail to admin panel

--- rollcall/admin.py
+++ rollcall/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from rollcall.models import Rollout
+from rollcall.models import Rollout, UserDetail
 
 @admin.register(Rollout)
 class RolloutAdmin(admin.ModelAdmin):
     fields = ('user', 'time')
     list_display = ('time', 'user')
+    
+@admin.register(UserDetail)
+class UserDetailAdmin(admin.ModelAdmin):
+    fields = ('user', 'name', 'personnel_code', 'unit', 'manager_name')
+    list_display = ('user', 'name', 'personnel_code')
