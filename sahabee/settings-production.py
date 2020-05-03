8bf9ea444c9f14d4be25d5623cd33b42e379eb10
emran <emran.bm@gmail.com>
2020-05-03 19:12:12 +0430

    Disable debug mode

--- sahabee/settings-production.py
+++ sahabee/settings-production.py
@@ -1,6 +1,6 @@
 from sahabee.settings import *
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dummy')
 DATABASES['default']['NAME'] = '/database/db.sqlite3'
