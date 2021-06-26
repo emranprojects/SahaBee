19ce5d1a86d05379ac41e509eefe6fe6b5523d2c
Emran <emran.bm@gmail.com>
2021-06-26 12:32:16 +0430

    Feat[front]: Add API link to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,16 @@
+import React from "react";
 import appPaths from "../appPaths";
 import {Navbar as BootstrapNavbar} from "react-bootstrap";
 import utils from "../utils";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faSignOutAlt} from '@fortawesome/free-solid-svg-icons'
+import apiURLs from "../apiURLs";
 
 export default function Navbar({isLoggedIn}) {
     return (
         <BootstrapNavbar className="nav-bar" bg="dark" expand="lg">
             <BootstrapNavbar.Brand href="/" className="text-white">SahaBee</BootstrapNavbar.Brand>
+            <a href={apiURLs.BASE_URL} target="_blank" className="btn btn-dark">API</a>
             {isLoggedIn ? <>
                     <a href={appPaths.dashboard} className="btn btn-dark">Dashboard</a>
                     <a href="/" className="btn btn-dark" onClick={utils.logout}>
