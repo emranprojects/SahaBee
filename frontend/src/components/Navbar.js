f66807b6a38f52c8c3e3f876e0d85e8b54a7eb48
Emran Batmanghelich <emran.bm@gmail.com>
2021-08-17 20:54:36 +0430

    Feat[front]: Add link to GitHub repo

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
 import appPaths from "../appPaths";
-import {Navbar as BootstrapNavbar} from "react-bootstrap";
+import {Nav, Navbar as BootstrapNavbar} from "react-bootstrap";
 import utils from "../utils";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faSignOutAlt} from '@fortawesome/free-solid-svg-icons'
+import {faSignOutAlt,} from '@fortawesome/free-solid-svg-icons'
 import apiURLs from "../apiURLs";
 import LoginContext from "./LoginContext";
+import githubIcon from "../static-media/GitHub-Mark-Light-32px.png"
 
 export default function Navbar() {
 
-    function logout(loginContext){
+    function logout(loginContext) {
         utils.logout()
         loginContext.setIsLoggedIn(false)
     }
@@ -17,18 +18,38 @@ export default function Navbar() {
     return (
         <LoginContext.Consumer>
             {loginContext =>
-                <BootstrapNavbar className="nav-bar" bg="dark" expand="lg">
-                    <BootstrapNavbar.Brand href="/" className="text-white">SahaBee</BootstrapNavbar.Brand>
-                    <a href={apiURLs.BASE_URL} target="_blank" className="btn btn-dark">API</a>
-                    {loginContext.isLoggedIn ? <>
-                            <a href={appPaths.dashboard} className="btn btn-dark">Dashboard</a>
-                            <a href={appPaths.userProfile} className="btn btn-dark">Profile</a>
-                            <a href="/" className="btn btn-dark" onClick={() => logout(loginContext)}>
-                                <abbr title="Logout"><FontAwesomeIcon icon={faSignOutAlt}/></abbr>
-                            </a>
-                        </> :
-                        <a href={appPaths.login} className="btn btn-dark">Login</a>
-                    }
+                <BootstrapNavbar className="nav-bar ml-auto" bg="dark" expand="lg">
+                    <Nav className="container-fluid">
+                        <Nav.Item>
+                            <BootstrapNavbar.Brand href="/" className="text-white">SahaBee</BootstrapNavbar.Brand>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <a href={apiURLs.BASE_URL} target="_blank" className="btn btn-dark">API</a>
+                        </Nav.Item>
+                        {loginContext.isLoggedIn ? <>
+                                <Nav.Item>
+                                    <a href={appPaths.dashboard} className="btn btn-dark">Dashboard</a>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <a href={appPaths.userProfile} className="btn btn-dark">Profile</a>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <a href="/" className="btn btn-dark" onClick={() => logout(loginContext)}>
+                                        <abbr title="Logout"><FontAwesomeIcon icon={faSignOutAlt}/></abbr>
+                                    </a>
+                                </Nav.Item>
+                            </> :
+                            <Nav.Item>
+                                <a href={appPaths.login} className="btn btn-dark">Login</a>
+                            </Nav.Item>
+                        }
+                        <Nav.Item className="ml-auto">
+                            <abbr title="GitHub repo">
+                            <a href="https://github.com/emranbm/SahaBee" target="_blank"
+                               className="btn btn-dark"><img src={githubIcon}/></a>
+                            </abbr>
+                        </Nav.Item>
+                    </Nav>
                 </BootstrapNavbar>
             }
         </LoginContext.Consumer>
