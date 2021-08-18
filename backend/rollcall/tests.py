2231640a8cb10c0b7dbe207f0c9d1efe9193c8a3
Emran Batmanghelich <emran.bm@gmail.com>
2021-08-19 01:48:54 +0430

    Test[back]: Clear sonar code smell
    Reuse '/users/self' string in UserUpdateTest

--- backend/rollcall/tests.py
+++ backend/rollcall/tests.py
@@ -114,6 +114,8 @@ class RolloutsAPITest(APITestCase):
 
 
 class UserUpdateTest(APITestCase):
+    SELF_USER_API_URL = '/users/self/' 
+
     def setUp(self) -> None:
         self.user = create_user()
         self.client.force_login(self.user)
@@ -121,7 +123,7 @@ class UserUpdateTest(APITestCase):
     def test_id_shouldnt_update(self):
         user_id = self.user.id
         detail_id = self.user.detail.id
-        self.client.put('/users/self/', data={
+        self.client.put(UserUpdateTest.SELF_USER_API_URL, data={
             'id': user_id + 1,
             'detail': {
                 'id': detail_id + 1,
@@ -132,7 +134,7 @@ class UserUpdateTest(APITestCase):
         self.assertEqual(self.user.detail.id, detail_id)
 
     def test_user_can_be_updated(self):
-        resp = self.client.put('/users/self/', data={
+        resp = self.client.put(UserUpdateTest.SELF_USER_API_URL, data={
             'username': 'new_test',
             'password': 'new_pass',
             'email': 'new_test@test.com',
@@ -144,7 +146,7 @@ class UserUpdateTest(APITestCase):
         self.assertEqual(self.user.email, 'new_test@test.com')
 
     def test_user_detail_can_be_updated(self):
-        resp = self.client.put('/users/self/', data={
+        resp = self.client.put(UserUpdateTest.SELF_USER_API_URL, data={
             'detail': {
                 'personnel_code': 'new_code',
                 'unit': 'new_unit',
@@ -164,6 +166,6 @@ class UserGetTest(APITestCase):
         self.client.force_login(self.user)
 
     def test_user_can_be_retrieved(self):
-        resp = self.client.get('/users/self/')
+        resp = self.client.get(UserUpdateTest.SELF_USER_API_URL)
         self.assertEqual(resp.status_code, HTTP_200_OK)
         self.assertEqual(resp.data['id'], self.user.id)
